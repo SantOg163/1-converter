ffafc954af19c9b53ca20aeea81fc17d03befa53
Самвел Сантурян <sant@MacBook-Air-Samvel.local>
2026-02-24 20:09:05 +0300

    [1-start] добавил базовый конвертер

--- main.go
+++ main.go
@@ -1,5 +1,11 @@
 package main
 
-func main(){
-	
+import "fmt"
+
+func main() {
+	const usdToEuro = 0.85
+	const usdToRub = 76.5
+	var rub = 115.0
+	result := (rub / usdToRub) * usdToEuro
+	fmt.Println("result = ", result)
 }
