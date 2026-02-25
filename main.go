6088276fa8e5ff7f277c2c77689a25220c9169cf
Самвел Сантурян <sant@Mac.ivstar.local>
2026-02-25 17:10:08 +0300

    Добавил ввод

--- main.go
+++ main.go
@@ -6,5 +6,16 @@ func main() {
 	const usdToEuro = 0.85
 	const usdToRub = 76.5
 	eurToRub := usdToRub / usdToEuro
+	input()
 	fmt.Println("eurToRub = ", eurToRub)
+}
+func input() (float64, string, string) {
+	count, from, to := 0.0, "usd", "eur"
+	fmt.Println("Введите число:")
+	fmt.Scan(&count)
+	fmt.Println("Введите исходную валюту:")
+	fmt.Scan(&from)
+	fmt.Println("Введите целеву/ валюту:")
+	fmt.Scan(&to)
+	return count, from, to
 }
