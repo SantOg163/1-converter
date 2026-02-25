5a007c3000e6fe6e99209cc25b1fc2a4a2253391
Самвел Сантурян <sant@Mac.ivstar.local>
2026-02-25 17:29:12 +0300

    Добавил ввод и конвертацию

--- main.go
+++ main.go
@@ -2,13 +2,17 @@ package main
 
 import "fmt"
 
+const usdToEur = 0.85
+const usdToRub = 76.5
+const eurToRub = usdToRub / usdToEur
+
 func main() {
-	const usdToEuro = 0.85
-	const usdToRub = 76.5
-	eurToRub := usdToRub / usdToEuro
-	input()
+	countFrom, from, to := input()
+	countTo := convert(countFrom, from, to)
 	fmt.Println("eurToRub = ", eurToRub)
+	fmt.Println(countTo)
 }
+
 func input() (float64, string, string) {
 	count, from, to := 0.0, "usd", "eur"
 	fmt.Println("Введите число:")
@@ -18,4 +22,35 @@ func input() (float64, string, string) {
 	fmt.Println("Введите целеву/ валюту:")
 	fmt.Scan(&to)
 	return count, from, to
+}
+
+func convert(amount float64, fromCurrency string, toCurrency string) float64 {
+	if fromCurrency == "rub" {
+		if toCurrency == "eur" {
+			return amount / eurToRub
+		}
+		if toCurrency == "usd" {
+			return amount / usdToRub
+		}
+	}
+
+	if fromCurrency == "usd" {
+		if toCurrency == "eur" {
+			return amount * usdToEur
+		}
+		if toCurrency == "rub" {
+			return amount * usdToRub
+		}
+	}
+
+	if fromCurrency == "eur" {
+		if toCurrency == "usd" {
+			return amount / usdToEur
+		}
+		if toCurrency == "rub" {
+			return amount * eurToRub
+		}
+	}
+
+	return amount
 }
